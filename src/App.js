1e51ce22615709c9d1045815768e48d9f1b7887e
cosisaxis <kishanelewis2@gmail.com>
2022-11-18 13:23:48 -0500

    galleria-init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ethers } from "ethers";
+import { useState } from "react";
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [data, setData] = useState([]);
+
+  const getNft = (_account) => {
+    const options = { method: "GET", headers: { accept: "application/json" } };
+
+    fetch(
+      `https://api.opensea.io/api/v1/collections?asset_owner=${_account}offset=0&limit=20`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        setData(response);
+         console.log(response)
+        })
+      .catch((err) => console.error(err));
+  };
+
+  const metaConnect = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    let res = await provider.send("eth_requestAccounts", []);
+    setAccount(res[0]);
+    getNft(res[0]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{account}</p>
+      <button onClick={metaConnect}>Connect</button>
+      <button onClick={getNft}>Get NFT Data</button>
     </div>
   );
 }
