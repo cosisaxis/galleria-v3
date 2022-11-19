660cf8ed6b0644aec635fac0177cbe805ff73ff6
cosisaxis <kishanelewis2@gmail.com>
2022-11-19 16:18:29 -0500

    testing-hooks

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import { useState } from "react";
 
 function App() {
   const [account, setAccount] = useState("");
-  const [data, setData] = useState([]);
+  const [data, setData] = useState("");
+ 
+  console.log(data);
+
+ 
+
 
   const getNft = (_account) => {
     const options = { method: "GET", headers: { accept: "application/json" } };
 
-    fetch(
-      `https://api.opensea.io/api/v1/collections?asset_owner=${_account}offset=0&limit=20`,
-      options
-    )
+    
+      fetch("https://api.opensea.io/api/v1/asset/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb/1/?include_orders=false", options)
+    
       .then((response) => response.json())
       .then((response) => {
-        setData(response);
+        setData(response.id);
          console.log(response)
         })
       .catch((err) => console.error(err));
@@ -26,14 +30,24 @@ function App() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     let res = await provider.send("eth_requestAccounts", []);
     setAccount(res[0]);
-    getNft(res[0]);
+    getNft(res);
   };
 
   return (
     <div className="App">
       <p>{account}</p>
       <button onClick={metaConnect}>Connect</button>
-      <button onClick={getNft}>Get NFT Data</button>
+      {/* <button onClick={getNft}>Get NFT Data</button> */}
+      
+      {Object.entries(data).map(([key, val], i) => {
+        return (
+          
+            <p key={i}>
+             {key} : {val}
+            </p>
+          
+        )
+      })}
     </div>
   );
 }
